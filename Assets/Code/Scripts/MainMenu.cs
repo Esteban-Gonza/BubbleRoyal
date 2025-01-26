dcc1dfd3c182e6efdd25d935dc7574cd160b2924
Esteban González <juanesgs2002@gmail.com>
2025-01-25 23:18:03 -0500

    TitleScene flow and instructions implemented

--- Assets/Code/Scripts/MainMenu.cs
+++ Assets/Code/Scripts/MainMenu.cs
@@ -9,13 +9,14 @@ public class MainMenu : MonoBehaviour
 {
     [SerializeField] private string gameScene;
 
-    [SerializeField] private Image transitionGameImg;
-    [SerializeField] private Image transitionImg;
+    [SerializeField] private GameObject mainMenuOB;
+    [SerializeField] private Transform transitionTransform;
+    [SerializeField] private Transform instructionsTransform;
     [SerializeField] private CanvasGroup creditsCG;
 
     private void Start()
     {
-        transitionGameImg.transform.DOLocalMoveX(-1920f, 1f).SetEase(Ease.InSine);
+        transitionTransform.DOLocalMoveX(-1920f, 1f).SetEase(Ease.InSine);
     }
 
     public void Exit()
@@ -30,14 +31,23 @@ public class MainMenu : MonoBehaviour
 
     private IEnumerator TransitionToGame()
     {
-        transitionGameImg.transform.DOLocalMoveX(0f, 1f).SetEase(Ease.InSine);
+        transitionTransform.DOLocalMoveX(0f, 1f).SetEase(Ease.InSine);
         yield return new WaitForSeconds(1f);
+        mainMenuOB.SetActive(false);
+        transitionTransform.DOLocalMoveX(-1920f, 1f).SetEase(Ease.InSine);
+        yield return new WaitForSeconds(1f);
+        instructionsTransform.DOLocalMoveX(960, 2f).SetEase(Ease.OutBounce);
+        yield return new WaitForSeconds(6f);
+        instructionsTransform.DOLocalMoveX(-960, 1.5f).SetEase(Ease.OutCubic);
+        yield return new WaitForSeconds(5f);
+        instructionsTransform.DOLocalMoveY(-1920, 1.5f).SetEase(Ease.InSine);
+        yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(gameScene);
     }
     
     private IEnumerator TransitionToExit()
     {
-        transitionImg.transform.DOLocalMoveX(0f, 1f).SetEase(Ease.InSine);
+        transitionTransform.DOLocalMoveX(0f, 1f).SetEase(Ease.InSine);
         yield return new WaitForSeconds(1f);
         Application.Quit();
     }
@@ -45,17 +55,12 @@ public class MainMenu : MonoBehaviour
     public void TransitionToCredits()
     {
         creditsCG.DOFade(1f, 1f);
-        transitionImg.transform.DOLocalMoveX(0, 1f).SetEase(Ease.InSine);
+        transitionTransform.DOLocalMoveX(0, 1f).SetEase(Ease.InSine);
     }
     
     public void TransitionFromCredits()
     {
         creditsCG.DOFade(0f, 1f);
-        transitionImg.transform.DOLocalMoveX(-1920, 1f).SetEase(Ease.InSine);
-    }
-
-    public void SetImageInSpace(Vector3 newLocation)
-    {
-        transitionImg.transform.position = newLocation;
+        transitionTransform.DOLocalMoveX(-1920, 1f).SetEase(Ease.InSine);
     }
 }
