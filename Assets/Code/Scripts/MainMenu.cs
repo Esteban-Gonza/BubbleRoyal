55e609219544317fa7539a2be035020cd0188ea8
Esteban González <juanesgs2002@gmail.com>
2025-01-30 20:39:14 -0500

    SkipTutorial feature

--- Assets/Code/Scripts/MainMenu.cs
+++ Assets/Code/Scripts/MainMenu.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using DG.Tweening;
+using TMPro;
+using UnityEngine.InputSystem.Controls;
 
 public class MainMenu : MonoBehaviour
 {
@@ -13,10 +15,31 @@ public class MainMenu : MonoBehaviour
     [SerializeField] private Transform transitionTransform;
     [SerializeField] private Transform instructionsTransform;
     [SerializeField] private CanvasGroup creditsCG;
+    [SerializeField] private TextMeshProUGUI skipTutorialTxt;
+
+    private bool tutorialOn;
+    private bool skipIsVisible;
 
     private void Start()
     {
+        skipTutorialTxt.text = "";
+        tutorialOn = false;
+        skipIsVisible = false;
         transitionTransform.DOLocalMoveX(-1920f, 1f).SetEase(Ease.InSine);
+    }
+
+    private void Update()
+    {
+        if(Input.anyKey && tutorialOn == true)
+        {
+            skipTutorialTxt.text = "Press X to skip";
+            skipIsVisible = true;
+        }
+        
+        if (Input.GetKey(KeyCode.X) && skipIsVisible == true && tutorialOn == true)
+        {
+            StartCoroutine(SkipTutorial());
+        }
     }
 
     public void Exit()
@@ -36,11 +59,20 @@ public class MainMenu : MonoBehaviour
         mainMenuOB.SetActive(false);
         transitionTransform.DOLocalMoveX(-1920f, 1f).SetEase(Ease.InSine);
         yield return new WaitForSeconds(1f);
+        tutorialOn = true;
         instructionsTransform.DOLocalMoveX(960, 2f).SetEase(Ease.OutBounce);
         yield return new WaitForSeconds(6f);
         instructionsTransform.DOLocalMoveX(-960, 1.5f).SetEase(Ease.OutCubic);
         yield return new WaitForSeconds(5f);
         instructionsTransform.DOLocalMoveY(-1920, 1.5f).SetEase(Ease.InSine);
+        yield return new WaitForSeconds(2f);
+        SceneManager.LoadScene(gameScene);
+    }
+
+    private IEnumerator SkipTutorial()
+    {
+        instructionsTransform.DOLocalMoveY(-1920, 1.5f).SetEase(Ease.InSine);
+        skipTutorialTxt.text = "";
         yield return new WaitForSeconds(2f);
         SceneManager.LoadScene(gameScene);
     }
